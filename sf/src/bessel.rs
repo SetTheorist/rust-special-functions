6e21ec4e7f22ac06ea2d1b83fd54a1b13bd4a90f
Apollo Hogan <apollo.hogan@gmail.com>
2021-09-19 15:16:48 -0400

    Bessel real symmetry tests

--- sf/src/bessel.rs
+++ sf/src/bessel.rs
@@ -71,6 +71,23 @@ impl BesselJ<isize> for r64 {
   }
 }
 
+mod tests{
+use super::*;
+use crate::real::*;
+#[test]
+  fn real_symmetry() {
+    assert_eq!(r64(1.0).bessel_j(0), r64(-1.0).bessel_j(0));
+
+    assert_eq!(r64(1.0).bessel_j(1), -r64(1.0).bessel_j(-1));
+    assert_eq!(r64(1.0).bessel_j(1), -r64(-1.0).bessel_j(1));
+    assert_eq!(r64(1.0).bessel_j(1), r64(-1.0).bessel_j(-1));
+
+    assert_eq!(r64(1.0).bessel_j(2), r64(1.0).bessel_j(-2));
+    assert_eq!(r64(1.0).bessel_j(2), r64(-1.0).bessel_j(2));
+    assert_eq!(r64(1.0).bessel_j(2), r64(-1.0).bessel_j(-2));
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 pub mod impls {
