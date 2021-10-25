162d4c7fca7f08963cada587a64da96516725ae5
Apollo Hogan <apollo.hogan@gmail.com>
2021-10-24 22:33:49 -0400

    f_arb ops, display (buggy), sqrt

--- sf/src/farb.rs
+++ sf/src/farb.rs
@@ -1,3 +1,4 @@
+use std::cmp::{Ord,Ordering,PartialOrd};
 use std::ops::{Add,Sub,Mul,Div,Neg,Rem};
 
 #[derive(Clone,Copy,Debug,Eq,Hash,Ord,PartialEq,PartialOrd)]
@@ -14,8 +15,8 @@ impl Default for Type {
 
 #[derive(Clone,Copy,Debug,Eq,Hash,Ord,PartialEq,PartialOrd)]
 pub enum Sign {
-  Positive,
   Negative,
+  Positive,
 }
 
 impl Neg for Sign {
@@ -34,24 +35,37 @@ impl Default for Sign {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Clone,Copy)]
-#[repr(transparent)]
+#[derive(Clone,Copy,PartialEq)]
 pub struct I<const N:usize>(pub [u32; N]);
+
+impl<const N:usize> I<N> {
+  #[inline]
+  fn leading_zeros(&self) -> u32 {
+    let mut z = 0;
+    for i in (0..N).rev() {
+      let c = self.0[i].leading_zeros();
+      z += c;
+      if c!=32 {break;}
+    }
+    z
+  }
+}
 
 impl<const N:usize> std::fmt::Debug for I<N> {
   fn fmt(&self, f:&mut std::fmt::Formatter) -> std::fmt::Result {
     write!(f, "[")?;
     for i in (0..N).rev() {
-      write!(f, " {:08x}", self.0[i])?;
+      //write!(f, "{}{:08x}", (if(i==N-1){""}else{"_"}), self.0[i])?;
+      write!(f, "{:08x}", self.0[i])?;
     }
-    write!(f, " ]")
+    write!(f, "]")
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // TODO: more efficient layout, etc.
-#[derive(Clone,Copy,Debug)]
+#[derive(Clone,Copy,Debug, PartialEq)]
 pub struct farb<const N:usize> {
   pub t:Type,
   pub s:Sign,
@@ -60,6 +74,41 @@ pub struct farb<const N:usize> {
 }
 
 impl<const N:usize> farb<N> {
+  pub fn ldexp(self, n:i32) -> farb<N> {
+    // TODO: types, underflow/overflow
+    let t = self.t;
+    let s = self.s;
+    let e = self.e + n;
+    let m = self.m;
+    farb { t, s, e, m }
+  }
+}
+
+impl<const N:usize> farb<N>
+  where [u32;2*N] : Sized
+{
+  // TODO: use iteration:
+  //  e = 1 - a*x
+  //  f = e*e + e
+  //  x' = f*x + x
+  // uses: x' = x*(2-a*x)
+  pub fn recip(self) -> farb<N> {
+    // TODO: types, etc.
+    let mut x = Self::from(f64::from(self).recip());
+    let t = Self::from(2.0_f32);
+    for _ in 0 .. (N.log2()+1) {
+      x = x*(t - self*x);
+    }
+    x
+  }
+
+  pub fn sqrt(self) -> farb<N> {
+    let mut z = Self::from(f64::from(self).sqrt());
+    for _ in 0 .. (N.log2()+1) {
+      z = (z + self*z.recip()).ldexp(-1);
+    }
+    z
+  }
 }
 
 // f16:  1: 5:(1): 10; bias 15, -14/+15
@@ -148,31 +197,74 @@ impl<const N:usize> From<farb<N>> for f64 {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+impl<const N:usize> std::fmt::Display for farb<N>
+  where [u32;2*N]:Sized
+{
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    let mut z = *self;
+    if z < Self::from(0.0) {
+      z = -z;
+      write!(f, "-")?;
+    }
+    let one = Self::from(1.0);
+    let ten = Self::from(10.0);
+    let tenth = ten.recip();
+    let mut e = 0;
+    while z >= ten {
+      e += 1;
+      z = z * tenth;
+    }
+    while z < one {
+      e -= 1;
+      z = z * ten;
+    }
+    for n in 0..(N*10) {
+      if n == 1 {
+        write!(f, ".")?;
+      }
+      let d = f64::from(z).floor();
+      if d<0.0 || d>=10.0 { print!("<<>>"); }
+      let dd = ((d as u8) + b'0') as char;
+      write!(f, "{}", dd)?;
+      z = (z - Self::from(d)) * ten;
+    }
+    if e != 0 { write!(f, "e{}", e)?; }
+    write!(f, "")
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 // normalize x to have msb=1 and rounded correctly (round-to-nearest, ties-to-even)
 // returns exponent adjustment
 pub fn normalize<const N:usize>(x:&mut I<N>, c:bool, low:u32) -> i32 {
-  println!(">> {:?}", x);
-  let mut ex = 0;
+  let mut ex : i32 = 0;
   let mut low = low;
+
+  // normalize to normal form
   if c {
     // shift 1 bit right
-    low = (low>>1)|(low&1)|(x.0[0]<<31);
-    for i in (0..(N-1)).rev() {
-      x.0[i] = (x.0[i]>>1) | (x.0[i+1]<<31);
-    }
-    x.0[N-1] = (x.0[N-1]>>1) | 0x8000_0000;
-    ex = 1;
+    let o = shr(x, 1);
+    x.0[N-1] |= 0x8000_0000;
+    low = (low>>1) | (low&1) | o;
+    ex += 1;
   } else {
-    let mut n = 0;
-    for i in (0..N).rev() {
-      let z = x.0[i].leading_zeros();
-      n += z;
-      if z != 0 { break; }
-    }
-    shl(x, n as u32);
+    let n = x.leading_zeros();
+    shl(x, &mut low, n as u32);
+    ex -= n as i32;
   }
-  println!("<< {:?}", x);
-  todo!()
+  // rounding (round-to-nearest, ties-to-even)
+  if low > 0x8000_0000
+    || (low == 0x8000_0000 && (x.0[0]&1 == 1)) {
+    let c = add_mi(x, 1);
+    if c {
+      let o = shr(x, 1);
+      x.0[N-1] |= 0x8000_0000;
+      //low = (low>>1) | (low&1) | o;
+      ex += 1;
+    }
+  }
+  ex
 }
 
 // adds in-place to x, returns true if carry
@@ -184,20 +276,88 @@ pub fn add_mm<const N:usize>(x:&mut I<N>, y:&I<N>) -> bool {
   c
 }
 
+// subtracts in-place to x, returns true if borrow
+pub fn sub_mm<const N:usize>(x:&mut I<N>, y:&I<N>) -> bool {
+  let mut c = false;
+  for i in 0..N {
+    (x.0[i], c) = x.0[i].borrowing_sub(y.0[i], c);
+  }
+  c
+}
+
+// adds in-place to x, returns true if carry
+pub fn add_mi<const N:usize>(x:&mut I<N>, y:u32) -> bool {
+  let mut c;
+  (x.0[0], c) = x.0[0].overflowing_add(1);
+  for i in 1..N {
+    if !c { break; }
+    (x.0[i], c) = x.0[i].overflowing_add(1);
+  }
+  c
+}
+
+// multiplies y in-place to x, returns carry word
+pub fn mul_mi<const N:usize>(x:&mut I<N>, y:u32) -> u32 {
+  let mut c = 0;
+  for i in 0..N {
+    (x.0[i], c) = x.0[i].carrying_mul(y, c);
+  }
+  c
+}
+
+// multiplies x,y into res
+pub fn mul_mm<const N:usize>(res:&mut I<{2*N}>, x:&I<N>, y:&I<N>) 
+  where [u32;2*N]:Sized
+{
+  for i in 0..N {
+    let mut c = 0;
+    for j in 0..N {
+      let xy;
+      (xy, c) = x.0[j].carrying_mul(y.0[i], c);
+      let c2;
+      (res.0[i+j], c2) = res.0[i+j].overflowing_add(xy);
+      if c2 {c+=1;}
+    }
+    res.0[i+N] += c;
+  }
+}
+
 // shifts-left in-place, discarding anything shifted off
-pub fn shl<const N:usize>(x:&mut I<N>, n:u32) -> () {
+// shifts extra low word in, except for lsb (sticky) bit
+pub fn shl<const N:usize>(x:&mut I<N>, low:&mut u32, n:u32) -> () {
   if n == 0 { return; }
   let nw = (n / 32) as usize;
   let nb = (n % 32) as usize;
   if nw >= N {
+    // entire value shifted away
     for i in 0..N {
       x.0[i] = 0;
     }
   } else {
+    // normal case
     for i in (1..(N-nw)).rev() {
-      x.0[i+nw] = (x.0[i] << nb) | (x.0[i-1] >> (32-nb));
+      x.0[i+nw] = (x.0[i] << nb) | (if nb>0 {x.0[i-1] >> (32-nb)} else {0});
     }
     x.0[nw] = (x.0[0] << nb);
+    for i in 0..nw {
+      x.0[i] = 0;
+    }
+  }
+  // deal with extra low word
+  if *low != 0 {
+    if nw <= N {
+      if nb > 0 {
+        x.0[nw] |= (*low&!1)>>(32-nb);
+      }
+      if nw > 0 {
+        x.0[nw-1] |= (*low&!1)<<nb;
+      }
+      if nw >= 1 {
+        *low &= 1;
+      } else if nw == 0 {
+        *low = (*low<<nb) | (*low&1);
+      }
+    }
   }
 }
 
@@ -239,7 +399,7 @@ pub fn shr<const N:usize>(x:&mut I<N>, n:u32) -> u32 {
     if nb > 0 { over |= x.0[nw] << (32-nb); }
     // normal shift
     for i in (0..(N-nw)) {
-      x.0[i] = (x.0[i+nw] >> nb) | (if i+nw<N-1 {(x.0[i+nw+1] << (32-nb))} else {0});
+      x.0[i] = (x.0[i+nw] >> nb) | (if i+nw<N-1 && nb>0 {(x.0[i+nw+1] << (32-nb))} else {0});
     }
     for i in (N-nw)..N {
       x.0[i] = 0;
@@ -250,9 +410,62 @@ pub fn shr<const N:usize>(x:&mut I<N>, n:u32) -> u32 {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
+impl<const N:usize> PartialEq<farb<N>> for farb<N> {
+  fn eq(&self, rhs:&farb<N>) -> bool {
+    // TODO!
+  }
+}
+*/
+
+impl<const N:usize> PartialOrd<farb<N>> for farb<N> {
+  fn partial_cmp(&self, rhs:&farb<N>) -> Option<Ordering> {
+    // type
+    if self.t != Type::Normal || rhs.t != Type::Normal {
+      // TODO!
+      return None;
+    }
+    // sign
+    if self.s < rhs.s {
+      return Some(Ordering::Less);
+    } else if self.s > rhs.s {
+      return Some(Ordering::Greater);
+    }
+    // exponent
+    if self.e < rhs.e {
+      return if self.s == Sign::Positive {Some(Ordering::Less)}
+        else {Some(Ordering::Greater)};
+    } else if self.e > rhs.e {
+      return if self.s == Sign::Positive {Some(Ordering::Greater)}
+        else {Some(Ordering::Less)};
+    }
+    // mantissa
+    for i in (0..N).rev() {
+      if self.m.0[i] > rhs.m.0[i] {
+        return Some(Ordering::Greater);
+      } else if self.m.0[i] < rhs.m.0[i] {
+        return Some(Ordering::Less);
+      }
+    }
+    return Some(Ordering::Equal);
+  }
+}
+
+impl<const N:usize> Neg for farb<N> {
+  type Output = farb<N>;
+  fn neg(self) -> farb<N> {
+    let t = self.t;
+    let s = -self.s;
+    let e = self.e;
+    let m = self.m;
+    farb { t, s, e, m }
+  }
+}
+
 impl<const N:usize> Add<farb<N>> for farb<N> {
   type Output = farb<N>;
   fn add(mut self, mut rhs:farb<N>) -> farb<N> {
+    if self.s != rhs.s { return self - (-rhs); }
     // TODO: special cases...
     if self.e >= rhs.e {
       let low = shr(&mut rhs.m, (self.e - rhs.e) as u32);
@@ -266,5 +479,83 @@ impl<const N:usize> Add<farb<N>> for farb<N> {
     } else {
        rhs.add(self)
     }
+  }
+}
+
+impl<const N:usize> Sub<farb<N>> for farb<N> {
+  type Output = farb<N>;
+  fn sub(mut self, mut rhs:farb<N>) -> farb<N> {
+    if self.s != rhs.s { return self + (-rhs); }
+    // TODO: special cases...
+    if self.e >= rhs.e {
+      let low = shr(&mut rhs.m, (self.e - rhs.e) as u32);
+      let c = sub_mm(&mut self.m, &rhs.m);
+      let ex = normalize(&mut self.m, c, low);
+      let t = self.t;
+      let s = if c {-self.s} else {self.s};
+      let e = self.e + ex;
+      let m = self.m;
+      farb { t, s, e, m }
+    } else {
+       -(rhs.sub(self))
+    }
+  }
+}
+
+impl<const N:usize> Mul<farb<N>> for farb<N>
+  where [u32;2*N] : Sized
+{
+  type Output = farb<N>;
+  fn mul(self, rhs:farb<N>) -> farb<N> {
+    // TODO: types; zero, other specials; etc.
+    let mut mx = I([0; 2*N]);
+    mul_mm(&mut mx, &self.m, &rhs.m);
+    let t = self.t;
+    let s = if self.s==rhs.s {Sign::Positive} else {Sign::Negative};
+    let mut e = self.e + rhs.e;
+    let mut m = I([0; N]);
+    let z = mx.leading_zeros();
+    if z < (N*32) as u32 {
+      let low  = shr(&mut mx, (N as u32 * 32) - z);
+      m.0.copy_from_slice(&mx.0[0..N]);
+      let ex = normalize(&mut m, false, low);
+      e += ex + ((N as i32 * 32) - (z as i32) - ((N as i32)*32-1));
+    } else if z > (N*32) as u32 {
+      if z == (2*N*32) as u32 {
+        return farb { t:Type::Zero, s, e:0, m:I([0;N]) };
+      }
+      todo!()
+    } else {
+      m.0.copy_from_slice(&mx.0[0..N]);
+    }
+    farb { t, s, e, m }
+  }
+}
+
+impl<const N:usize> Mul<farb<N>> for u32 {
+  type Output = farb<N>;
+  #[inline] fn mul(self, rhs:farb<N>) -> farb<N> { rhs*self }
+}
+
+impl<const N:usize> Mul<u32> for farb<N> {
+  type Output = farb<N>;
+  fn mul(mut self, rhs:u32) -> farb<N> {
+    if rhs == 0 {
+      return farb { t:Type::Zero, s:self.s, e:0, m:I([0;N]) };
+    } else if rhs == 1 {
+      return self;
+    }
+    // TODO: zero, subnormal, types, etc.
+    // for normal, non-zero values, c is never zero
+    let c = mul_mi(&mut self.m, rhs);
+    let z = c.leading_zeros();
+    let low = shr(&mut self.m, 32-z);
+    if z<32 { self.m.0[N-1] |= c << z; }
+    let ex = normalize(&mut self.m, false, low);
+    let t = self.t;
+    let s = self.s;
+    let e = self.e + ex + ((32-z) as i32);
+    let m = self.m;
+    farb { t, s, e, m }
   }
 }
