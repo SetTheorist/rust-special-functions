ce97a363d36896822d9d8451f987e081ea4db2cc
Apollo Hogan <apollo.hogan@gmail.com>
2022-06-23 22:53:48 -0400

    fill out exp

--- sf/src/exp.rs
+++ sf/src/exp.rs
@@ -2,15 +2,31 @@ use sf_impl::exp;
 use sf_impl::real::*;
 
 pub trait SFExp {
-  fn sf_exp(self) -> Self;
+  // TODO: does this must_use annotation in a trait actually do anything?
+  #[must_use] fn sf_exp(self) -> Self;
+  #[must_use] fn sf_exp_m1(self) -> Self;
+  #[must_use] fn sf_exp_m1vx(self) -> Self;
+  #[must_use] fn sf_expn(self, n:isize) -> Self;
+  #[must_use] fn sf_exp_men(self, n:isize) -> Self;
+  #[must_use] fn sf_exp_menx(self, n:isize) -> Self;
 }
 
 /// compute sf_exp(x) $` {} = \exp(x) = e^x `$ 
 /// ```math
 /// {} = \sum_{n=0}^\infty \frac{x^n}{n!}
 /// ```
-#[inline] pub fn sf_exp<V:SFExp>(x:V) -> V { x.sf_exp() }
+#[must_use]#[inline] pub fn sf_exp<V:SFExp>(x:V) -> V { x.sf_exp() }
+#[must_use]#[inline] pub fn sf_exp_m1<V:SFExp>(x:V) -> V { x.sf_exp_m1() }
+#[must_use]#[inline] pub fn sf_exp_m1vx<V:SFExp>(x:V) -> V { x.sf_exp_m1vx() }
+#[must_use]#[inline] pub fn sf_expn<V:SFExp>(n:isize, x:V) -> V { x.sf_expn(n) }
+#[must_use]#[inline] pub fn sf_exp_men<V:SFExp>(n:isize, x:V) -> V { x.sf_exp_men(n) }
+#[must_use]#[inline] pub fn sf_exp_menx<V:SFExp>(n:isize, x:V) -> V { x.sf_exp_menx(n) }
 
 impl SFExp for f64 {
-  #[inline] fn sf_exp(self) -> Self { exp::sf_exp(r64(self)).0 }
+  #[must_use]#[inline] fn sf_exp(self) -> Self { exp::sf_exp(r64(self)).0 }
+  #[must_use]#[inline] fn sf_exp_m1(self) -> Self { exp::sf_exp_m1(r64(self)).0 }
+  #[must_use]#[inline] fn sf_exp_m1vx(self) -> Self { exp::sf_exp_m1vx(r64(self)).0 }
+  #[must_use]#[inline] fn sf_expn(self, n:isize) -> Self { exp::sf_expn(n, r64(self)).0 }
+  #[must_use]#[inline] fn sf_exp_men(self, n:isize) -> Self { exp::sf_exp_men(n, r64(self)).0 }
+  #[must_use]#[inline] fn sf_exp_menx(self, n:isize) -> Self { exp::sf_exp_menx(n, r64(self)).0 }
 }
