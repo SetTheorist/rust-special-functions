5b8f3c3d8a553e356c1ffb21302d2b523d51d5e6
Apollo Hogan <apollo.hogan@gmail.com>
2022-06-25 22:57:01 -0400

    .

--- sf/benches/sf_benchmark.rs
+++ sf/benches/sf_benchmark.rs
@@ -4,16 +4,25 @@ use sf::airy::*;
 use sf::exp::*;
 
 fn benchmark(c: &mut Criterion) {
-  let mut group = c.benchmark_group("Airy");
-  for x in [0.1, 1.0, 5.0, 10.0, 50.0].iter() {
-    group.bench_with_input(BenchmarkId::new("AI", x), |b,&x| b.iter(|| sf_airy_ai(x)));
-    group.bench_with_input(BenchmarkId::new("BI", x), |b,&x| b.iter(|| sf_airy_bi(x)));
-    group.bench_with_input(BenchmarkId::new("AIBI", x), |b,&x| b.iter(|| sf_airy_aibi(x)));
+  {
+    let mut group = c.benchmark_group("Airy");
+    for x in [0.1, 1.0, 5.0, 10.0, 50.0].iter() {
+      group.bench_with_input(BenchmarkId::new("AI", x), x, |b,&x| b.iter(|| sf_airy_ai(x)));
+      group.bench_with_input(BenchmarkId::new("BI", x), x, |b,&x| b.iter(|| sf_airy_bi(x)));
+      group.bench_with_input(BenchmarkId::new("AIBI", x), x, |b,&x| b.iter(|| sf_airy_aibi(x)));
+    }
   }
 
-  let mut group = c.benchmark_group("exp");
-  for x in [0.1, 0.5, 1.0, 5.0, 10.0].iter() {
-    group.bench_with_input(BenchmarkId::from_parameter(x), x, |b,&x|b.iter(||sf_exp(x)));
+  {
+    let mut group = c.benchmark_group("exp");
+    for x in [0.1, 0.5, 1.0, 5.0, 10.0].iter() {
+      group.bench_with_input(BenchmarkId::new("exp", x), x, |b,&x|b.iter(||sf_exp(x)));
+      group.bench_with_input(BenchmarkId::new("exp_m1", x), x, |b,&x|b.iter(||sf_exp_m1(x)));
+      group.bench_with_input(BenchmarkId::new("exp_m1vx", x), x, |b,&x|b.iter(||sf_exp_m1vx(x)));
+      //group.bench_with_input(BenchmarkId::new("expn(5)", x), x, |b,&x|b.iter(||sf_expn(5, x)));
+      //group.bench_with_input(BenchmarkId::new("exp_men(5)", x), x, |b,&x|b.iter(||sf_exp_men(5, x)));
+      //group.bench_with_input(BenchmarkId::new("exp_menx(5)", x), x, |b,&x|b.iter(||sf_exp_menx(5, x)));
+    }
   }
 }
 
